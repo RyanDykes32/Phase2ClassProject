657d56715c485809eb149dbde65c77a1b0a0e529
Ryan Dykes <ryan.dykes@marquette.edu>
2023-12-04 12:51:01 -0600

    Transfer of Code from one project to another

---
+++ ServiceClassProject/Model/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceClassProject.Model;
+
+    public class Account
+    {
+        public static Dictionary<string, string> loginPairs = new Dictionary<string, string>();//Key = Username, Value = Password
+        public static Dictionary<string, Account> accountList = new Dictionary<string, Account>(); //Key = Username, Value = Account
+
+        protected string username;
+        protected string password;
+    }
---
+++ ServiceClassProject/Model/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceClassProject.Model
+{
+    public class Car
+    {
+        private static int uniqueLicenseNumber;
+
+        private int licenseID;
+        private string make;
+        private string model;
+        private int year;
+        private float priceRate;
+        public Dictionary<Date, Customer> schedule;
+
+        public Car(int id, string ma, string mo, int y, float pr)
+        {
+            licenseID = id;
+            make = ma;
+            model = mo;
+            year = y;
+            priceRate = pr;
+            schedule = new Dictionary<Date, Customer>();
+        }
+
+        public void ReserveCar(Customer c, Date start, Date end)
+        {
+            if (!CheckAvailability(start, end))
+            {
+                Console.WriteLine("Error: Attempt to Reserve Unavailable Block");
+                return;
+            }
+            List<Date> list = Date.GetDatesBetween(start, end);
+            foreach (var l in list)
+            {
+                schedule.Add(l, c);
+            }
+
+        }
+
+        public bool CheckAvailability(Date start, Date end)
+        {
+            List<Date> list = Date.GetDatesBetween(start, end);
+            foreach (var l in list)
+            {
+                if (schedule.ContainsKey(l))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+
+
+
+
+    }
+
+
+
+}
