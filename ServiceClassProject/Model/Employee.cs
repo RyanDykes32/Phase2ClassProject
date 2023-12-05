5975235b1edad486f619d08103aca30c193583a0
Ryan Dykes <ryan.dykes@marquette.edu>
2023-12-05 13:22:09 -0600

    Employee Account with username and password

--- ServiceClassProject/Model/Employee.cs
+++ ServiceClassProject/Model/Employee.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace ServiceClassProject.Model;
 
-public class Employee
+public class Employee : Account
 {
+    // Constructor for creating a Customer object with a specified username and password.
+    public Employee(string na, string pa)
+    {
+        // Initialize the username and password fields with the provided values.
+        username = na;
+        password = pa;
 
+        // Add the username and password to the loginPairs dictionary.
+        // This allows the system to verify login credentials during the authentication process.
+        loginPairs.Add(username, password);
 
+        // Add the username and the corresponding Customer object to the accountList dictionary.
+        // This enables quick access to the Customer object associated with a given username.
+        accountList.Add(username, this);
 
-
-}
+    }
+    }
