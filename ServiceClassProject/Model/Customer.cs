7e253509da550da3cd5155f32c7a9877ec5e10e5
Ryan Dykes <ryan.dykes@marquette.edu>
2023-12-05 12:54:48 -0600

    Included Comments to the provided code

--- ServiceClassProject/Model/Customer.cs
+++ ServiceClassProject/Model/Customer.cs
@@ -8,11 +8,20 @@ namespace ServiceClassProject.Model;
 
 public class Customer : Account
 {
+    // Constructor for creating a Customer object with a specified username and password.
     public Customer(string na, string pa)
     {
+        // Initialize the username and password fields with the provided values.
         username = na;
         password = pa;
+
+        // Add the username and password to the loginPairs dictionary.
+        // This allows the system to verify login credentials during the authentication process.
         loginPairs.Add(username, password);
+
+        // Add the username and the corresponding Customer object to the accountList dictionary.
+        // This enables quick access to the Customer object associated with a given username.
         accountList.Add(username, this);
     }
+
 }
--- ServiceClassProject/Model/Date.cs
+++ ServiceClassProject/Model/Date.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 
 namespace ServiceClassProject.Model
 {
+    // Struct representing a date with month, day, and year components.
     public struct Date
     {
         private int month;
         private int day;
         private int year;
 
+        // Constructor to initialize a Date object with provided month, day, and year values.
         public Date(int m, int d, int y)
         {
             month = m;
@@ -19,21 +21,25 @@ namespace ServiceClassProject.Model
             year = y;
         }
 
+        // Method to get the month component of the date.
         public int GetMonth()
         {
             return month;
         }
 
+        // Method to get the day component of the date.
         public int GetDay()
         {
             return day;
         }
 
+        // Method to get the year component of the date.
         public int GetYear()
         {
             return year;
         }
 
+        // Method to get the next day in the date sequence.
         public Date GetNextDay()
         {
             if (day < DateTime.DaysInMonth(year, month))
@@ -50,11 +56,13 @@ namespace ServiceClassProject.Model
             }
         }
 
+        // Method to check if two dates are equal.
         public bool IsEqual(Date d)
         {
             return d.GetDay() == day && d.GetMonth() == month && d.GetYear() == year;
         }
 
+        // Static method to get a list of dates between a start and end date.
         public static List<Date> GetDatesBetween(Date start, Date end)
         {
             List<Date> returnList = new List<Date>();
@@ -67,18 +75,19 @@ namespace ServiceClassProject.Model
             return returnList;
         }
 
+        // Method to print the date in the format: month/day/year.
         public void PrintDate()
         {
             Console.Write(month + "/" + day + "/" + year);
-
         }
 
+        // Method to print the date in the format: month/day/year followed by a newline.
         public void PrintDateLine()
         {
             Console.WriteLine(month + "/" + day + "/" + year);
-
         }
 
+        // Static method to print a list of dates.
         public static void PrintDates(List<Date> dl)
         {
             foreach (var d in dl)
@@ -88,5 +97,7 @@ namespace ServiceClassProject.Model
             }
         }
     }
+
 }
+       
 
